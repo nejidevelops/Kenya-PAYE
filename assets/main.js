68a578e8c81a13a781b543e8decf44891f53a765
NewtonOmbese <newtonombese1@gmail.com>
2022-07-05 18:15:28 +0300

    enhancing more dom manip

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,13 @@
-let month = document.getElementById("month")
-let year = document.getElementById("year")
-let salary= document.getElementById("salanje")
-let benefit = document.getElementById("benefinje")
-let nhifY = document.getElementById("deductY")
-let nhifN = document.getElementById("deductN")
-let nssfY = document.getElementById("newRates")
-let nssfN = document.getElementById("oldRates")
-let calculate = document.getElementById("calculateTax")
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+let salary= document.getElementById("salanje");
+let benefit = document.getElementById("benefinje");
+let nhifY = document.getElementById("deductY");
+let nhifN = document.getElementById("deductN");
+let nssfY = document.getElementById("newRates");
+let nssfN = document.getElementById("oldRates");
+let calculate = document.getElementById("calculateTax");
+let calculateTax;
 
 calculate.addEventListener('click',)
 
